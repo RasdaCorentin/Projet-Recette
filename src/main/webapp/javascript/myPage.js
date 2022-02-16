5f7449162c1cd483ae1271fcce2dc4f1477503d9
Dok974 <rasdacorentin@gmail.com>
2022-02-16 12:33:24 +0100

    Update main 16/02
    Resolution des problemes d'affichage suite au merge

--- src/main/webapp/javascript/myPage.js
+++ src/main/webapp/javascript/myPage.js
@@ -2,3 +2,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
+let form = document.getElementById("myForm");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var url = 'http://localhost:8080/Projet-Recette/api/utilisateur/recette/liste/1';
+    var headers = new Headers();
+    var username = document.getElementById("nom").value;
+    var password = document.getElementById("password").value;
+    var authBasic = username + ":" + password;
+    async function fetchData() {
+        //headers.append('Content-Type', 'application/json');
+        headers.set('Authorization', 'Basic ' + btoa(authBasic));
+
+
+        let response = await fetch(url, {method: 'GET',
+            headers: headers
+        });
+        console.log(response.status); // 200
+        console.log(response.statusText); // OK
+        if (response.status === 201) {
+            let data = await response.json();
+            console.log(data);
+            var table = document.getElementById("ListRecettes")
+
+            for (let i in data) {
+                var text = "<tr>"
+                text += "<td>" + data[i].libelle + "</td>"
+                        + "<td>" + data[i].description + "</td>"
+                text += "</tr>";
+                table.innerHTML += text;
+            }
+        }
+    }
+    fetchData();
+});
