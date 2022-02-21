d84251a4b96b05a910b0cf1e0a658c41575ac450
Yoann Bezard <yoannbezard0@gmail.com>
2022-02-21 12:10:24 +0100

    Début de l'intégration de cipher.
    Intégration de cipher dans les fichiers connexion et inscription .js.

--- src/main/webapp/javascript/connexion.js
+++ src/main/webapp/javascript/connexion.js
@@ -3,27 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
 
-/**********************************
- 
- Methode PUT Formulaire Connexion
- 
- ***********************************/
+/*
+***********************************
+% Méthode PUT Formulaire Connexion
+************************************
+*/
 
-//Methode d'écoute sur Formulaire
+//= Méthode d'écoute sur Formulaire.
 var form = document.getElementById("myForm");
-//Mise en place des variable pour la connection à la BDD
+
+//= Mise en place des variable pour la connection à la BDD.
 var http = new XMLHttpRequest();
-//Url api
+
+//= Url api.
 var url = 'http://localhost:8080/Projet-Recette/api/utilisateur/enregistrez/connect';
 var method = 'PUT';
 
-
-
-
-//Ecoute bouton submit
+//? Écoute du bouton submit.
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    //J'initialise les variable à envoyer à l'API
+
+    //= J'initialise les variable à envoyer à l'API.
     var nom = document.getElementById("nom").value;
     var password = document.getElementById("password").value;
 
@@ -33,31 +33,60 @@ form.addEventListener("submit", function (event) {
             "password": password
         }
     };
-    //Je change data en JSON & lance fonction requestTest
-    //(je passe le mot de passe ici car j'en ai besoin pour construire le cookie)
+
+    //= Je change data en JSON & lance fonction requestTest.
+    //= (je passe le mot de passe ici car j'en ai besoin pour construire le cookie)
     data = JSON.stringify(data);
     requestTest(data, password);
 
     //§ Renvoie l'utilisateur vers la page d'accueil.
     location.href = "index.html"
 });
-//Envois + Réponse de l'API en JSON
+
+//? Envois + Réponse de l'API en JSON.
 function requestTest(data, password) {
     http.open(method, url);
     http.setRequestHeader('Content-Type', 'application/json');
+
     http.onreadystatechange = function () {
         if (http.readyState === XMLHttpRequest.DONE && http.status === 202) {
             var res = JSON.parse(http.responseText);
-            //Ici je récupére la réponse en JSON que je met dans var nom & id 
+
+            //= Ici je récupère la réponse en JSON que je met dans var nom & id.
             res = JSON.parse(http.responseText);
-            // et je crée le cookie qui ressemble à "utilisateur=nom:paswword:id" réutilisé uniquement dans cookie.js
+
+            //§ et je crée le cookie qui ressemble à "utilisateur=nom:password:id" réutilisé uniquement dans cookie.js
             var nom = res.nom;
             var id = res.id;
+
+            //= La constante cipher, qui va permettre de hacher le mot de passe.
+            const cipher = salt => {
+                const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+                const byteHex = n => ("0" + Number(n).toString(16)).substr(-2);
+                const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
+            
+                return text => text.split('')
+                    .map(textToChars)
+                    .map(applySaltToChar)
+                    .map(byteHex)
+                    .join('');
+            }
+
+            //= Je donne le salt pour hacher le mot de passe.
+            const myCipher = cipher(nom);
+
+            //= Je hache le mot de passe.
+            var passwordCipher = myCipher(password);
+
             //§ Création du cookie "utilisateur", pour permettre la connexion de l'utilisateur sur toutes les pages.
-    document.cookie = "utilisateur=" + nom + ":" + password + ":" + id +"; path=/; max-age=31536000 ; samesite=lax"; //= Ce cookie à une durée de vie d'un an.
+            document.cookie = "utilisateur=" + nom + ":" + passwordCipher + ":" + id +"; path=/; max-age=31536000 ; samesite=lax"; //= Ce cookie à une durée de vie d'un an.
+
+            alert("Cookie:" + document.cookie);
         } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 202) {
             console.log("Error");
         }
     };
+
     http.send(data);
+
 };
--- src/main/webapp/javascript/inscription.js
+++ src/main/webapp/javascript/inscription.js
@@ -3,25 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
 
-/********************************
- 
- Methode Post Formulaire Inscription
- 
- *******************************/
+/*
+****************************************
+% Méthode Post Formulaire Inscription.
+****************************************
+*/
 
-
-//Methode d'écoute sur Formulaire
+//= Méthode d'écoute sur Formulaire
 var form = document.getElementById("myForm");
-//Mise en place des variable pour la connection à la BDD
+
+//= Mise en place des variable pour la connection à la BDD
 var http = new XMLHttpRequest();
-//Url api
+
+//= Url api
 var url = 'http://localhost:8080/Projet-Recette/api/utilisateur/enregistrez';
 var method = 'POST';
 
-//Ecoute bouton submit
+//? Écoute d'un bouton submit.
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    //J'initialise les variable à envoyer à l'API
+
+    //= J'initialise les variable à envoyer à l'API.
     var data = {
         "utilisateur": {
             "nom": document.getElementById("nom").value,
@@ -29,32 +31,60 @@ form.addEventListener("submit", function (event) {
             "email": document.getElementById("email").value
         }
     };
-    /*******************************************************************
-     Je prend une seconde fois le mot de passe pour le mettre dans le cookie
-     **********************************************************************/
+
+    /*
+    ***************************************************************************
+    % Je prend une seconde fois le mot de passe pour le mettre dans le cookie.
+    ***************************************************************************
+    */
     var password = document.getElementById("password").value;
-    //Je change data en JSON & lance fonction requestTest 
-    //(je passe le mot de passe ici car j'en ai besoin pour construire le cookie)
+
+    //= Je change data en JSON & lance fonction requestTest.
+    //= (je passe le mot de passe ici car j'en ai besoin pour construire le cookie)
     data = JSON.stringify(data);
     requestTest(data, password);
-    
-        //§ Renvoie l'utilisateur vers la page d'accueil.
+
+    //§ Renvoie l'utilisateur vers la page d'accueil.
     location.href = "index.html"
 });
 
-//Envois + Réponse de l'API en JSON
+//? Envois + Réponse de l'API en JSON.
 function requestTest(data, password) {
+
     http.open(method, url);
     http.setRequestHeader('Content-Type', 'application/json');
+
     http.onreadystatechange = function () {
         if (http.readyState === XMLHttpRequest.DONE && http.status === 201) {
-            //Ici je récupére la réponse en JSON que je met dans var nom & id 
+            //= Ici je récupère la réponse en JSON que je met dans var nom & id 
             res = JSON.parse(http.responseText);
-            // et je crée le cookie qui ressemble à "utilisateur=nom:paswword:id" réutilisé uniquement dans cookie.js
+
+            //= et je crée le cookie qui ressemble à "utilisateur=nom:password:id" réutilisé uniquement dans cookie.js.
             var nom = res.nom;
             var id = res.id;
+
+            //= La constante cipher, qui va permettre de hacher le mot de passe.
+            const cipher = salt => {
+                const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+                const byteHex = n => ("0" + Number(n).toString(16)).substr(-2);
+                const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
+            
+                return text => text.split('')
+                    .map(textToChars)
+                    .map(applySaltToChar)
+                    .map(byteHex)
+                    .join('');
+            }
+
+            //= Je donne le salt pour hacher le mot de passe.
+            const myCipher = cipher(nom);
+
+            //= Je hache le mot de passe.
+            var passwordCipher = myCipher(password);
+
             //§ Création du cookie "utilisateur", pour permettre la connexion de l'utilisateur sur toutes les pages.
-    document.cookie = "utilisateur=" + nom + ":" + password + ":" + id +"; path=/; max-age=31536000 ; samesite=lax"; //= Ce cookie à une durée de vie d'un an.
+            document.cookie = "utilisateur=" + nom + ":" + passwordCipher + ":" + id +"; path=/; max-age=31536000 ; samesite=lax"; //= Ce cookie à une durée de vie d'un an.
+
             alert("Cookie:" + document.cookie);
         } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 201) {
             console.log("Error");
@@ -65,4 +95,3 @@ function requestTest(data, password) {
     alert(data);
 
 };
-// document.cookie = "utilisateur=Corentin:123:1"
